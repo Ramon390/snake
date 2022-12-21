7b783234916fd1299ba59c3f84376e6d4628be86
Ramon390 <31714424+Ramon390@users.noreply.github.com>
2022-12-21 16:57:22 +0800

    Update 源.cpp

--- Project1/源.cpp
+++ Project1/源.cpp
@@ -1,18 +1,18 @@
-﻿/* 贪吃蛇游戏 */
+/* 贪吃蛇游戏 贡献者：Ramon、 */
 #include <iostream>
+#include <time.h>
 using namespace std;
-int dollarX, dollarY;
+int dollarX, dollarY, maxMapSize = 12;//地图最大边缘
 int main()
 {
     srand(time(0));//产生随机种子
-    dollarX = rand() % 10 + 2;//范围为：2~11
-    dollarY = rand() % 10 + 2;
+    dollarX = rand() % (maxMapSize - 2) + 2;//随机坐标x
+    dollarY = rand() % (maxMapSize - 2) + 2;//随机坐标y
     cout << "dollar: "<< dollarX << "," << dollarY << endl;
-    int sizeMax = 12;//地图最大边缘
     //输出
-    for (int i = 1; i <= sizeMax; ++i) {
-        for (int j = 1; j <= sizeMax; ++j) {
-            if (i == 1 || i == sizeMax || j==1 || j==sizeMax)
+    for (int i = 1; i <= maxMapSize; ++i) {
+        for (int j = 1; j <= maxMapSize; ++j) {
+            if (i == 1 || i == maxMapSize || j==1 || j==maxMapSize)
                 cout << "*";//输出边界
             else if (dollarX == i && dollarY == j) {
                 cout << "$";
